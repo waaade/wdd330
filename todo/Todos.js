6891decf989447a3132e5dc7241a9714bb4f1fdb
waaade <johnlarson562@gmail.com>
2020-05-30 13:55:55 -0600

    Most functionality implemented, untested

--- todo/Todos.js
+++ todo/Todos.js
@@ -1,6 +1,46 @@
+import * as ls from "ls.js";
+import * as utilities from "utilities";
 export default class Todos {
     constructor(elementId, key) {
         this.elementId = elementId;
         this.key = key;
+        listTodos();
     }
+
+    function addTodo() {
+        const task = Document.getElementById("new-item").value;
+        saveTodo(task, "myList");
+        listTodos();
+    }
+
+    function listTodos() {
+        const element = Document.getElementById("list");
+        renderTodoList(getTodos("myList"), element);
+    }
+}
+
+let todoList = null;
+
+function saveTodo(task, key) {
+    let date = new Date();    
+    let todo = { id:date.getTime(), content:task, completed:false }
+    //Add one item to list
+    todoList.push(todo);
+    //Save entire list to localStorage
+    ls.writeToLS(key, todoList);
+}
+
+function getTodos(key) {
+    if (todoList == null) {
+        let items = ls.readFromLS(key);
+        todoList = items;
+    }
+    return todoList;
+}
+
+function renderTodoList(list, element) {
+    element.innerHTML = "";
+    list.forEach(item => {
+        element.append("<li>'item.content'</li>");
+    });
 }
--- todo/main.js
+++ todo/main.js
@@ -1,2 +1,7 @@
 "use strict";
-const form = document.getElementById("add-task");
+import Todos from "Todos.js";
+
+const toDo = new Todos('todo', 'list');
+
+//make it so addTodo function will be called when button is touched.
+toDo.onTouch("#add", addTodo);
