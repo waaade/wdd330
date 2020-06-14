0b287fef2bc0d4bd9a4598322321057f5427f821
waaade <johnlarson562@gmail.com>
2020-06-13 20:31:47 -0600

    To-do list fully working

--- todo/Todos.js
+++ todo/Todos.js
@@ -2,6 +2,7 @@ import * as ls from "./ls.js";
 import * as utilities from "./utilities.js";
 
 let todoList = null;
+let display = 0;
 
 export default class Todos {
     constructor(elementId, key) {
@@ -11,14 +12,30 @@ export default class Todos {
         utilities.onTouch("#add", this.addTodo);
     }
     listTodos() {
-        renderTodoList(getTodos("myList"), this.elementId);
+        renderTodoList(getTodos("myList"), this.elementId);        
     }
     addTodo() {
         const task = document.getElementById("new-item").value;
         saveTodo(task, "myList");
         this.listTodos();
     }
+
     
+}
+
+ function filter() {
+        utilities.onTouch("#all", ()=>{
+            display = 0;
+            renderTodoList(getTodos("myList"), utilities.qs("#list"));
+        });
+        utilities.onTouch("#active", ()=>{
+            display = 1;
+            renderTodoList(getTodos("myList"), utilities.qs("#list"));
+        });
+        utilities.onTouch("#complete", ()=>{
+            display = 2;
+            renderTodoList(getTodos("myList"), utilities.qs("#list"));
+    });
 }
 
 function saveTodo(task, key) {
@@ -54,15 +71,19 @@ function renderTodoList(list, element) {
         col1.innerHTML = "";
         list.forEach(item => {
             let complete = document.createElement("div");
-              
-                if (item.completed) {
-                    complete.innerHTML = "<p>✓</p>"
-                }
-                else {
-                    complete.innerHTML = "<p>◯</p>";
-                }
+            //Render based on current filter
+            //Display completed ones in all and completed
+            if (item.completed && display != 1) {
+                complete.innerHTML = "<p>✓</p>";
                 complete.id = `c${item.id}`;                    
                 col1.appendChild(complete);
+
+            }
+            else if (!item.completed && display != 2) {
+                complete.innerHTML = "<p>O</p>";
+                complete.id = `c${item.id}`;                    
+                col1.appendChild(complete);
+            }
             
         });
 
@@ -71,13 +92,17 @@ function renderTodoList(list, element) {
         col2.innerHTML = "";
         list.forEach(item => {
             let content = document.createElement("div");
-            if (item.completed) {
+            if (item.completed && display != 1) {
                 content.innerHTML = `<p><del>${item.content}</del></p>`;
+                col2.appendChild(content);
+
             }
-            else {
-                content.innerHTML = `<p>${item.content}</p>`;
+            else if (display != 2) {
+                if (!item.completed) {
+                    content.innerHTML = `<p>${item.content}</p>`;
+                    col2.appendChild(content);
+                }
             }
-            col2.appendChild(content);
         });
         //The delete button
         let col3 = document.getElementById("col3");
@@ -85,9 +110,13 @@ function renderTodoList(list, element) {
         list.forEach(item => {
             let remove = document.createElement("div");
             remove.innerHTML = "<p>X</p>";
-            remove.id = `r${item.id}`;               
-            col3.appendChild(remove);
-
+            remove.id = `r${item.id}`;
+            if (item.completed && display != 1) {
+                col3.appendChild(remove);
+            }
+            else if (!item.completed && display != 2) {
+                col3.appendChild(remove);
+            }                    
         });
         addCompleteListeners(list);
         addRemoveListeners(list);
@@ -99,15 +128,32 @@ function renderTodoList(list, element) {
         footer.innerHTML = "0 tasks left";
     }
     else {
-        footer.innerHTML = countNotDone(list) + " tasks left";
-        }
-        
-        
+        footer.innerHTML = "<div>" + countNotDone(list) + " tasks left</div>"; 
     }
+    footer.innerHTML += "<div id='filters'><span id='all'>All </span><span id='active'>Active </span><span id='complete'>Complete</span></div>";
+
+    switch (display) {
+        case 0:
+            document.getElementById("all").style.border = "2px solid dodgerblue";
+            break;
+        case 1:
+            document.getElementById("active").style.border = "2px solid dodgerblue";
+            break;
+        case 2:
+            document.getElementById("complete").style.border = "2px solid dodgerblue";
+            break;
+        default:
+            break;
+    }
+
+    filter();
+
+}
 
 function addCompleteListeners(list) {
     list.forEach(item => {
-        utilities.onTouch(`#c${item.id}`, ()=> {
+        if (utilities.qs(`#c${item.id}`) != null) {
+            utilities.onTouch(`#c${item.id}`, ()=> {
             if (item.completed) {
                         item.completed = false;
                     }
@@ -117,18 +163,22 @@ function addCompleteListeners(list) {
             ls.writeToLS("myList", list);
             renderTodoList(getTodos("myList"), utilities.qs("#list"));
             });
+        }
     });
 }
 
 function addRemoveListeners(list) {
-    for (let i = 0; i < list.length; i++) {
-           utilities.onTouch(`#r${list[i].id}`, ()=> { 
+    for (let i = 0; i < list.length; i++) {   
+        if (utilities.qs(`#r${list[i].id}` != null)) {
+                utilities.onTouch(`#r${list[i].id}`, ()=> { 
                 list.splice(i, 1);
                 ls.writeToLS("myList", list);
                 renderTodoList(getTodos("myList"), utilities.qs("#list"));
-           });         
+            });   
+        }
      }
 }
+
 
 function countNotDone(list) {
     let count = 0;
