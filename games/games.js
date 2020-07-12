0efd001be441eb7632294b74055846eda85bcc6a
waaade <johnlarson562@gmail.com>
2020-07-11 20:02:21 -0600

    stub functions, style

---
+++ games/games.js
@@ -0,0 +1,24 @@
+// Displays the main menu
+function displayMenu() {
+
+}
+
+// Displays a local list of games
+function displayList(listName) {
+
+}
+
+// Displays the results of a query
+function displaySearch(array) {
+
+}
+
+// Queries for new releases
+function showNew() {
+    
+}
+
+// Queries for upcoming games
+function showFuture() {
+
+}
